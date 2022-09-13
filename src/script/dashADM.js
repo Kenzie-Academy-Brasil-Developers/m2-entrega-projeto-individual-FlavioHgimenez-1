ce1478eb4fa96b764133f999430407c54757776b
Daniel Hermenegildo Gimenez <attackhu3@gmail.com>
2022-09-13 01:17:39 -0300

    Entrega s7-02 js

--- src/script/dashADM.js
+++ src/script/dashADM.js
@@ -92,7 +92,9 @@ class DashBoardAdmin{
         const descricao = document.createElement("p")
         const horarioAbrir = document.createElement("p")
         const setor = document.createElement("p")
-  
+        
+        this.clickCardsEmpresas(li, element)
+        
         nomeEmpresa.innerText = element.name
         descricao.innerText = element.description
         horarioAbrir.innerText = `Abre: ${element.opening_hours}`
@@ -110,9 +112,10 @@ class DashBoardAdmin{
         main.append(h1, ul_buttons_empresa, ul)
       })
     })    
-}
+    
+  }
 
-static async cadastrarEmpresaApi(body){
+  static async cadastrarEmpresaApi(body){
   const user = await fetch('http://localhost:6278/companies', {
     method: "POST",
     headers: {
@@ -123,9 +126,9 @@ static async cadastrarEmpresaApi(body){
     .then(res => res)
 
     return user;
-}
+  }
 
-static async setoresApi(){
+  static async setoresApi(){
   const data = await fetch('http://localhost:6278/sectors', {
       "headers": {Authorization: `Bearer ${this.token}`}})
       .then(resp => resp.json())
@@ -133,7 +136,7 @@ static async setoresApi(){
       .catch(err => err);
 
       return data;
-}
+  }
 
   static async modalCriarEmpresa(){
     const main = document.querySelector(".main")
@@ -170,7 +173,7 @@ static async setoresApi(){
         sector_uuid: inputSetor.value
       }
       this.cadastrarEmpresaApi(empresa)
-      window.alert("Empresa Criada com sucesso!!!")
+      window.location.reload()
       divModal.remove()
       divFade.remove()
     })
@@ -223,9 +226,208 @@ static async setoresApi(){
     main.append(divFade, divModal)
   }
 
-}
+  static clickCardsEmpresas(li, element){
+    li.addEventListener("click", event =>{
+      event.preventDefault()
+      this.modalLi(element)
+    })
+  }
 
+  static async modalLi(element){
+    const main = document.querySelector(".main")
+    const divModal = document.createElement("div")
+    const modal = document.createElement("div")
+    const fade = document.createElement("div")
+    const nomeEmpresa = document.createElement("h1")
+
+    nomeEmpresa.innerText = element.name
+    nomeEmpresa.classList = "nomeEmpresa"
+
+    fade.addEventListener("click", event=>{
+      event.preventDefault()
+      if(event.target === fade){
+        divModal.remove(fade, modal)
+      }
+    })
+    
+    divModal.classList = "divModal"
+    modal.classList = "modal_empresas"
+    fade.classList = "fade_empresas"
+    
+    const buttonDepartamentos = document.createElement("button")
+    const buttonCriarDepartamentos = document.createElement("button")
+
+    buttonDepartamentos.addEventListener("click", event=>{
+      event.preventDefault()
+      divModal.innerHTML = "" 
+      this.departamentoModal(element.uuid)
+    })
+    buttonCriarDepartamentos.addEventListener("click", event =>{
+      event.preventDefault()
+      divModal.innerHTML = ""
+      this.criarModalDepartamento(element.uuid)
+    })
+
+    buttonDepartamentos.innerText = "Departamentos"
+    buttonCriarDepartamentos.innerText = "Criar departamento"
+
+    buttonDepartamentos.classList = "modal_empresas_button_departamentos"
+    buttonCriarDepartamentos.classList = "modal_empresas_button_criarDepartamentos"
+
+    main.appendChild(divModal)
+    divModal.append(fade, modal)
+    modal.append(nomeEmpresa, buttonDepartamentos, buttonCriarDepartamentos)
+    
+  }
+
+  static clickSair(){
+    const buttonSair = document.querySelector(".button_sair")
+    buttonSair.addEventListener("click", event=>{
+      event.preventDefault()
+      window.location.replace("/index.html")
+    })
+  }
+
+  static async listarTodosUsersApi(){
+    const users = await fetch("http://localhost:6278/users", {
+    method: "GET",
+    headers: this.headers})
+    .then(res => res.json())
+    .then(res => {return res})
+
+    return users
+  }
+
+  static async usersSemDepartamentoApi(){
+    const usersSemDepartamento = await fetch("http://localhost:6278/admin/out_of_work", {
+    method: "GET",
+    headers: this.headers})
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  
+    return usersSemDepartamento
+  }
+
+  static async listarTodosDepartamentosApi(uuid){
+    const departaments = await fetch(`http://localhost:6278/departments/${uuid}`, {
+      method: "GET",
+      headers: this.headers})
+      .then(res => res.json())
+      .then(res => {return res})
+      
+      return departaments
+  }
+
+  static async departamentoModal(uuid){
+    const divModal = document.querySelector(".divModal")
+    const modal = document.createElement("div")
+    const fade =  document.createElement("div")
+    const labelFuncionario = document.createElement("label")
+    const selectFuncuinario = document.createElement("select")
+    const buttonDepartamento = document.createElement("button")
+
+    modal.classList = "modal_funcionario"
+    fade.classList = "fade_funcionario"
+
+    buttonDepartamento.innerText = "Ver departamento"
+    buttonDepartamento.classList = "button_departamento"
+    labelFuncionario.for = "contract"
+    labelFuncionario.innerText = "Departamentos"
+    selectFuncuinario.name = "contract"
+    selectFuncuinario.id = "contract"
+    selectFuncuinario.required = true
+
+    const departamentos = await this.listarTodosDepartamentosApi(uuid);
+
+    if(departamentos.length === 0){
+      const nenhumDepartamento = document.createElement("option")
+      nenhumDepartamento.innerHTML = "Nenhum departamento encontrado"
+      selectFuncuinario.appendChild(nenhumDepartamento);
+      buttonDepartamento.disabled = true
+    }else{
+      departamentos.forEach(element => {
+        const option = document.createElement("option");
+        option.value     = element.uuid;
+        option.innerHTML = element.description;
+        selectFuncuinario.appendChild(option);
+    })
+    }
+
+    fade.addEventListener("click", event=>{
+      event.preventDefault()
+      if(event.target === fade){
+        divModal.remove(fade, modal)
+      }
+    })
+  
+    modal.append(labelFuncionario, selectFuncuinario, buttonDepartamento)
+    divModal.append(modal, fade)
+  }
+
+  static async criarModalDepartamento(uuid){
+    const divModal = document.querySelector(".divModal")
+    const h1CriarDepartamento = document.createElement("h1")
+    const modal = document.createElement("div")
+    const fade =  document.createElement("div")
+    const h2Nome = document.createElement("h2")
+    const inputNome = document.createElement("input")
+    const h2Descricao = document.createElement("h2")
+    const inputDescricao = document.createElement("input")
+    const buttonCriarDepartamento = document.createElement("button")
+
+    h2Nome.innerText = "Nome"
+    inputNome.placeholder = "Digite o nome do departamento"
+    h2Descricao.innerText = "Descrição"
+    inputDescricao.placeholder = "Digite a descrição do departamento"
+    buttonCriarDepartamento.innerText = "criar"
+    buttonCriarDepartamento.classList = "buttonCriarDepartamento"
+
+    h1CriarDepartamento.classList = "criarDepartamento"
+    h1CriarDepartamento.innerText = "Criar departamento"
+    h2Nome.classList = "h2Nome"
+    inputNome.classList = "inputNome"
+    inputNome.required = true
+    h2Descricao.classList = "h2Descricao"
+    inputDescricao.classList = "inputDescricao"
+    inputDescricao.required = true
+    modal.classList = "modal_departamento"
+    fade.classList = "fade_departamento"
+
+    fade.addEventListener("click", event=>{
+      event.preventDefault()
+      if(event.target === fade){
+        divModal.remove(fade, modal)
+      }
+    })
+
+    modal.append(h1CriarDepartamento, h2Nome, inputNome, h2Descricao, inputDescricao, buttonCriarDepartamento)
+    divModal.append(modal, fade)
+    
+    const criarDepartamento = {
+      name: inputNome.value,
+      description: inputDescricao.value,
+      company_uuid: uuid
+    }
+
+    // this.criarDepartamentoApi(criarDepartamento)
+    
+
+  }
+
+  static async criarDepartamentoApi(body){
+    const criandoDepartamento = await fetch("http://localhost:6278/departments", {
+    method: "POST",
+    headers: this.headers,
+    body: JSON.stringify(body)})
+    .then(res => res.json())
+    .then(res => {return res})
+
+    return criandoDepartamento
+  }
+}
 
 DashBoardAdmin.setoresApi()
 DashBoardAdmin.criandoSetor()
 DashBoardAdmin.criandoPaginaEmpresas()
+DashBoardAdmin.clickSair()
+
